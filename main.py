6f98a6a51cd05110e0361ba94ebbe367e622a665
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-04-18 11:43:32 -0700

    Testing Bio SeqIO

---
+++ main.py
@@ -0,0 +1,9 @@
+from Bio import SeqIO
+input_file = open("NIHMS926583-supplement-2.fasta", "r")
+output_file = open("output.txt", "w")
+
+for seq_record in SeqIO.parse(input_file, "fasta"):
+    print(seq_record.id)
+    print(seq_record.seq)
+    print(len(seq_record))
+
