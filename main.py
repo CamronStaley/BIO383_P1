c50203ecf0734b6f053447e3e645ba7d4df7ad77
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-04-24 12:03:06 -0700

    cleaning up

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from Bio import SeqIO
-import os
-import sn
-from boto import sns
 import functionCalls
 
-black_directory = "Data/Black Hair"
-bh_data = functionCalls.parse_file(black_directory)
+bh_directory = "Data/Black Hair"
+bh_data = functionCalls.parse_file(bh_directory)
 print(bh_data[0])
