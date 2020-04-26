aa632ee690c24941adf6dfcf5070a8e257197501
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-04-25 17:32:02 -0700

    fixing csv

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ bh_data = functionCalls.parse_file(bh_directory)
 functionCalls.display_table(bh_data)
 
 # only run if you want a cvs file
-# functionCalls.data_to_cvs('bh_datafile.csv', bh_data)
+functionCalls.data_to_cvs(bh_data, 'bh_datafile.csv')
 
