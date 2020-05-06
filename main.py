8f2cc26bfb00c4080fd09c145191c3cd3f292a19
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-05-05 20:00:37 -0700

    adding blond and brown hair

--- main.py
+++ main.py
@@ -3,43 +3,49 @@ from functionCalls import Genotype
 
 bh_directory = "Data/Black Hair"
 rh_directory = "Data/Red Hair"
+brh_directory = "Data/Brown Hair"
+blh_directory = "Data/Blonde Hair"
+
 black_hair = FunctionCalls()
 red_hair = FunctionCalls()
+brown_hair = FunctionCalls()
+blonde_hair = FunctionCalls()
+
 
 black_hair.parse_file(bh_directory)
 red_hair.parse_file(rh_directory)
-
-black_hair.display_table()
-red_hair.display_table()
+brown_hair.parse_file(brh_directory)
+blonde_hair.parse_file(blh_directory)
 
 black_hair.count_genotypes()
 red_hair.count_genotypes()
-
-print("Black Hair")
-black_hair.display_total_genos()
-print()
-print("Red Hair")
-red_hair.display_total_genos()
-print()
+brown_hair.count_genotypes()
+blonde_hair.count_genotypes()
 
 black_hair.count_geno_by_rsid()
 red_hair.count_geno_by_rsid()
-
-print("Black Hair")
-black_hair.display_geno_by_rsid()
-print()
-print("Red Hair")
-red_hair.display_geno_by_rsid()
-print()
+brown_hair.count_geno_by_rsid()
+blonde_hair.count_geno_by_rsid()
 
 black_hair.get_most_occur()
 red_hair.get_most_occur()
+brown_hair.get_most_occur()
+blonde_hair.get_most_occur()
 
 print("Black Hair")
 black_hair.display_most_occur()
 print()
+
 print("Red Hair")
 red_hair.display_most_occur()
+print()
+
+print("Brown Hair")
+brown_hair.display_most_occur()
+print()
+
+print("Blonde Hair")
+blonde_hair.display_most_occur()
 print()
 
 black_hair.compare_results("Black", red_hair, "Red")
