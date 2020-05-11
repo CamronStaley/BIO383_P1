5c0b46e344056dde802890887b5dd56ab5b49e8b
Camron Staley <55006027+CamronStaley@users.noreply.github.com>
2020-05-11 15:36:16 -0700

    adding more genotypes so there are 16

--- functionCalls.py
+++ functionCalls.py
@@ -20,11 +20,17 @@ class Genotype:
             'CC': 0,
             'GG': 0,
             'AT': 0,
+            'TA': 0,
             'AC': 0,
+            'CA': 0,
             'AG': 0,
+            'GA': 0,
             'GT': 0,
+            'TG': 0,
             'CT': 0,
-            'CG': 0
+            'TC': 0,
+            'CG': 0,
+            'GC': 0
         }
 
 
